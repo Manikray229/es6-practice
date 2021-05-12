5fcc203706e2d29a1336d4891938800416dd0175
Manik Ray <manikchandraray229@gmail.com>
2021-05-12 22:12:56 +0600

    adding tamlate

---
+++ tamplate.js
@@ -0,0 +1,16 @@
+const firstName = "Justin";
+const secondName = "Beiber";
+const fullName = firstName + " " + secondName + " is a good singer";
+const fullName2 = `${firstName} ${secondName} ${56+455+565} is a good singer`;
+console.log(fullName2); 
+
+const multiLine = "i love you\n"
++ "i kiss you\n"
++ "i habi ghabi you\n";
+
+console.log(multiLine);
+
+const multiLine2 = ` i love you 
+i kiss you 
+i habi ghabi you`;
+console.log(multiLine2)
